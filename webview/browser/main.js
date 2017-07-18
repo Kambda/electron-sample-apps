5e7dec0a2cf54abebc3eec73fb70de4d29ef51b0
Silvia Charpentier <scharpentier@kambda.com>
2017-07-18 16:34:53 -0600

    find-concept

--- webview/browser/main.js
+++ webview/browser/main.js
@@ -8,6 +8,8 @@ app.on('window-all-closed', function() {
 
 app.on('ready', function() {
   mainWindow = new BrowserWindow({width: 1024, height: 768 });
+  mainWindow.maximize();
+  mainWindow.setMenu(null);
   mainWindow.loadURL('file://' + __dirname + '/browser.html');
-  mainWindow.openDevTools();
+  //mainWindow.openDevTools();
 });
